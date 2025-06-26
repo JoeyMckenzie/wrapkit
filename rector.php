9d9a8778e0420f23d3928ec27bb73a67d06f0689
Joey McKenzie <joey.mckenzie27@gmail.com>
2025-06-26 09:15:22 -0700

    chore: update rector rules

--- rector.php
+++ rector.php
@@ -2,9 +2,7 @@
 
 declare(strict_types=1);
 
-use Rector\CodeQuality\Rector\Class_\InlineConstructorDefaultToPropertyRector;
 use Rector\Config\RectorConfig;
-use Rector\TypeDeclaration\Rector\ClassMethod\AddVoidReturnTypeWhereNoReturnRector;
 
 return RectorConfig::configure()
     ->withPaths([
@@ -12,15 +10,6 @@ return RectorConfig::configure()
         __DIR__.'/tests',
     ])
     ->withPhpSets(php84: true)
-    ->withRules([
-        AddVoidReturnTypeWhereNoReturnRector::class,
-        InlineConstructorDefaultToPropertyRector::class,
-    ])
-    ->withPreparedSets(
-        deadCode: true,
-        codeQuality: true,
-        typeDeclarations: true,
-        privatization: true,
-        earlyReturn: true,
-        strictBooleans: true,
-    );
+    ->withTypeCoverageLevel(10)
+    ->withDeadCodeLevel(10)
+    ->withCodeQualityLevel(10);
